fa76d036b75c098663dc31116a26b8b57b38bad6
Matheus Valandro <matheusvalandro@MacBook-Pro-de-Matheus.local>
2021-07-04 12:30:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<img alt="Ignite" src=".github/cover-react-native.svg" style="margin-bottom: 30px;" />
+
+<h1 align="left">
+  Desafio 01: Conceitos do React Native
+</h1>
+
+<p>
+Nesse desafio, você deverá criar uma aplicação para treinar o que aprendeu até agora no React Native.
+
+Essa será uma aplicação de lembrete de tarefas, onde você vai treinar um pouco mais sobre manipulação do estado no React.
+As funcionalidades do aplicativo são:
+
+- Adicionar uma nova tarefa
+- Remover uma tarefa
+- Marcar e desmarcar uma tarefa como concluída
+</p>
+
+## Running
+
+```bash
+# install dependencies
+yarn
+
+# install iOS dependencies
+cd ios && pod install
+
+# start application
+yarn ios
+
+# run tests
+yarn test
+```
+
+## Environment
+
+- Node.js v14.17.2
+- Xcode Version 12.5.1
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,36 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (!newTaskTitle) {
+      return;
+    }
+
+    setTasks(oldState => [
+      ...oldState,
+      { id: new Date().getTime(), title: newTaskTitle, done: false },
+    ]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    setTasks(oldState => {
+      return oldState.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            done: !task.done,
+          };
+        }
+
+        return task;
+      });
+    });
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => [...oldState.filter(task => task.id !== id)]);
   }
 
   return (
@@ -31,11 +49,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
